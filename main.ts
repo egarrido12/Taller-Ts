787cc586137a8444f21fc744803b008f0c9fa2bf
egarrido12 <111029178+egarrido12@users.noreply.github.com>
2025-04-06 23:09:16 -0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { series } from './data.js';
+import { Serie } from './Serie.js';
+
+const tableBody: HTMLElement = document.querySelector("tbody")!;
+
+let totalSeasons = 0;
+
+series.forEach((serie) => {
+  totalSeasons += serie.seasons;
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${serie.id}</td>
+    <td>${serie.name}</td>
+    <td>${serie.channel}</td>
+    <td>${serie.seasons}</td>
+  `;
+  tableBody.appendChild(row);
+});
+
+const avgSeasons = (totalSeasons / series.length).toFixed(2);
+
+const avgRow = document.createElement("tr");
+avgRow.innerHTML = `
+  <td colspan="4"><strong>Promedio de temporadas: ${avgSeasons}</strong></td>
+`;
+tableBody.appendChild(avgRow);
